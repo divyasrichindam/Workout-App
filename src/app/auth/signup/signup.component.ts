eb322baeae45d7cff994e437b818d3a4e0e9ba64
Divya Sri <divyasrichindam02@gmail.com>
2021-04-13 13:32:51 -0500

    created all the components

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field'
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@NgModule({
+    imports: [MatButtonModule,MatIconModule,MatFormFieldModule],
+    exports: [MatButtonModule,MatIconModule,MatFormFieldModule]
+}) 
+export class MaterialModule {}
